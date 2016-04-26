4630f3725302a74b44824edc6a92024df6d86258
Heinrich Löwen <loewen.heinrich@gmail.com>
2016-04-26 17:57:34 +0200

    Implement basic settings functionality.

--- SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/main/MainActivity.java
+++ SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/main/MainActivity.java
@@ -1,9 +1,11 @@
 package de.ifgi.sc.smartcitiesapp.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import de.ifgi.sc.smartcitiesapp.R;
 
@@ -13,12 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    // --- Menu ---
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_item_settings:
+                // Open the settings activity
+                Intent intentSettings = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intentSettings);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/main/SettingsFragment.java
@@ -0,0 +1,31 @@
+package de.ifgi.sc.smartcitiesapp.main;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.ifgi.sc.smartcitiesapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
