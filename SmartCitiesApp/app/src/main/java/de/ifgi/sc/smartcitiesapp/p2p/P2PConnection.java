ab7f5bbf76a70e11c40ce7dfaab250f134d00df0
Heinrich Löwen <loewen.heinrich@gmail.com>
2016-05-08 21:07:18 +0200

    Restrict p2p interface to p2p connection

--- SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/p2p/P2PConnection.java
+++ SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/p2p/P2PConnection.java
@@ -1,11 +1,11 @@
-package de.ifgi.sc.smartcitiesapp.interfaces;
+package de.ifgi.sc.smartcitiesapp.p2p;
 
 import de.ifgi.sc.smartcitiesapp.messaging.Message;
 
 /**
  * Created by helo on 26/04/16.
  */
-public interface Connection {
+public interface P2PConnection {
 
     /**
      * To be called by the Messenger to get a Message shared
--- SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/p2p/P2PManager.java
+++ SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/p2p/P2PManager.java
@@ -1,12 +1,11 @@
-package de.ifgi.sc.smartcitiesapp.server;
+package de.ifgi.sc.smartcitiesapp.p2p;
 
-import de.ifgi.sc.smartcitiesapp.interfaces.Connection;
 import de.ifgi.sc.smartcitiesapp.messaging.Message;
 
 /**
  * Created by helo on 7/05/16.
  */
-public class ServerConnection implements Connection {
+public class P2PManager implements P2PConnection {
 
     @Override
     public void shareMessage(Message m) {
