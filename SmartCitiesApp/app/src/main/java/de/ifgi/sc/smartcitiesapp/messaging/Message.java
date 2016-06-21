49c86007073bf7a29f1f88271e48f81221f9459f
apurvaakochar <apurvaakochar@gmail.com>
2016-06-21 20:17:45 +0200

    Removed ClientID

--- SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/messaging/Message.java
+++ SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/messaging/Message.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public class Message {
 
-    private String client_ID;
+
     private String message_ID;
     private String zone_ID;
     private String cr_Dt;
@@ -20,8 +20,8 @@ public class Message {
     private double longitude;
     private SimpleDateFormat D_format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
-    public Message(String c_id, String m_id, String z_id, Date crDt, double lat,double lon, Date exDt, String top, String tit, String msg){
-        client_ID=c_id;
+    public Message(String m_id, String z_id, Date crDt, double lat,double lon, Date exDt, String top, String tit, String msg){
+
         message_ID=m_id;
         zone_ID=z_id;
         cr_Dt = changeDateFormat(crDt);
@@ -31,12 +31,10 @@ public class Message {
         topic=top;
         title=tit;
         message=msg;
-        Log.i("Msg "+c_id, " Created ");
+
     }
 
-    public void setClient_ID(String id){
-        client_ID=id;
-    }
+
     public void setMessage_ID(String id){
         message_ID=id;
     }
@@ -53,7 +51,7 @@ public class Message {
     public void setTitle(String tit){   title=tit;}
     public void setMsg(String   m){message=m;}
 
-    public String getClient_ID(){return client_ID;};
+
     public String getMessage_ID(){return message_ID;};
     public String getZone_ID(){return zone_ID;};
     public String getCreated_At(){return cr_Dt;};
