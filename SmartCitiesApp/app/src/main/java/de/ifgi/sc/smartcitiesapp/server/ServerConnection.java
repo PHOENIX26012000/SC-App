10b68154521239b09facf8cef4bbcb609daa9da2
shahzeib <shahzeibjaswal@live.com>
2016-07-08 16:28:52 +0200

    Async server

--- SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/server/ServerConnection.java
+++ SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/server/ServerConnection.java
@@ -58,49 +58,11 @@ public class ServerConnection implements Connection{
      */
 
     public void shareMessage(ArrayList<Message> messages) {
-        /*
-        this.messages = messages;
-         */
-       // jsonObject = jsonParser.parseMessagetoJSON(messages);
-        ///JSONArray share = new JSONArray();
-        //share.put(jsonObject);
+
         new PostMsgTask().execute("http://giv-project6.uni-muenster.de:8080/api/addmessages");
 
     }
-       /* URL url = null;
-        HttpURLConnection client = null;
-        try {
-            url = new URL("http://giv-project6.uni-muenster.de:8080/api/addmessages");
-            client = (HttpURLConnection) url.openConnection();
 
-            client.setRequestMethod("POST");
-            client.connect();
-            Log.d("MSG", "Connection Successful");
-            client.setRequestProperty("Key","Value");
-            client.setDoOutput(true);
-            client.setChunkedStreamingMode(0);
-            OutputStreamWriter out = new OutputStreamWriter(client.getOutputStream());
-            //writeStream(out);
-            out.write(jsonObject.toString());
-            out.flush();
-            out.close();
-
-            Log.d("MSG","Message Sent");
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if(client != null) // Make sure the connection is not null.
-                client.disconnect();
-        }
-
-
-
-    */
 
 
     /**
@@ -111,93 +73,12 @@ public class ServerConnection implements Connection{
         //todo request to server for Messages as JSONArray
     }
 
-        /*try{
-        URL obj = new URL("http://giv-project6.uni-muenster.de:8080/api/messages/");
-        HttpURLConnection conn = null;
-
-            conn = (HttpURLConnection) obj.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            Log.d("MSG","Connected to Server");
-            conn.setRequestProperty("Key","Value");
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            BufferedReader buff= new BufferedReader(new InputStreamReader(in));
-            StringBuilder responseStrBuilder = new StringBuilder();
-
-            String inputStr;
-            while((inputStr=buff.readLine())!=null)
-                responseStrBuilder.append(inputStr);
-
-            //String result = buff.readLine();
-            //JSONArray newJ= new JSONArray();
-            //newJ.put(result);
-            JSONObject jsonObject = new JSONObject(responseStrBuilder.toString());
-
-            messages = jsonParser.parseJSONtoMessage(jsonObject);
-            //todo call Messanger and push Messages
-            Log.d("MSG","Successful");
-            {
-                if (conn != null);
-                conn.disconnect();
-            }
-        }
-
-
-        catch (JSONException e) {
-            throw new RuntimeException(e);
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } */
 
     public void getZones() {
 
         new GetZoneTask().execute("http://giv-project6.uni-muenster.de:8080/api/zones");
 
     }
-     /*   URL obj = null;
-        try {
-            obj = new URL("http://giv-project6.uni-muenster.de:8080/api/zones");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpURLConnection zoneCon = null;
-        try{
-            zoneCon = (HttpURLConnection) obj.openConnection();
-            zoneCon.setRequestMethod("GET");
-            zoneCon.connect();
-            zoneCon.setRequestProperty("Key","Value");
-            InputStream in = new BufferedInputStream(zoneCon.getInputStream());
-            BufferedReader buff= new BufferedReader(new InputStreamReader(in));
-            StringBuilder responseStrBuilder = new StringBuilder();
-
-            String inputStr;
-            while((inputStr=buff.readLine())!=null)
-                responseStrBuilder.append(inputStr);
-
-
-
-            JSONObject jsonObject = new JSONObject(responseStrBuilder.toString());
-
-            jsonParser.parseJSONtoZone(jsonObject);
-        }
-
-        catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        finally {
-            if (zoneCon != null);
-            zoneCon.disconnect();
-        }
-    }
-*/
     /**
      * Created by Shahzeib on 7/5/2016.
      */
@@ -222,6 +103,7 @@ public class ServerConnection implements Connection{
                 conn.setDoOutput(true);
                 conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 
+                conn.connect();
                 this.messages= messages;
                 jsonObject=jsonParser.parseMessagetoJSON(messages);
 
@@ -230,7 +112,7 @@ public class ServerConnection implements Connection{
                 out.write(jsonObject.toString());
                 out.close();
 
-                conn.connect();
+
                 int response = conn.getResponseCode();
                 if (response >= 200 && response <=399){
                     is = conn.getInputStream();
@@ -263,12 +145,14 @@ public class ServerConnection implements Connection{
             return response+responseString;
 
         }
-
-        //protected void onPostExecute(String result){
+/*
+        protected void onPostExecute(String result){
         // data submitted successfully?
-        //if (result.contains("200")) {
-        // maybe add other codes as well such as 201, or all between >=200 <400
+        if (result.contains("201")) {
 
+        }
+        // maybe add other codes as well such as 201, or all between >=200 <400
+*/
     }
 
     public class GetMsgTask extends AsyncTask<String,Integer, String> {
