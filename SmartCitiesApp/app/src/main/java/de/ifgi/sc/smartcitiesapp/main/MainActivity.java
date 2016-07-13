70bdac192314356113a4f7606b2f0d0016d09884
Heinrich Loewen <loewen.heinrich@gmail.com>
2016-07-13 15:20:48 +0200

    bugfix

--- SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/main/MainActivity.java
+++ SmartCitiesApp/app/src/main/java/de/ifgi/sc/smartcitiesapp/main/MainActivity.java
@@ -72,15 +72,19 @@ public class MainActivity extends AppCompatActivity implements MessagesObtainedL
         if (app == null)
             app = (App) getApplication();
 
+        // add the MessagesObtainedListener to the UIMessageManager:
+        UIMessageManager.getInstance().setMessageObtainedListener(this);
+
         // Start P2P Messaging
         mP2PManager = new P2PManager(this);
 
+        /*
         // Testing
         ArrayList<de.ifgi.sc.smartcitiesapp.messaging.Message> mPubMessagesTest = new ArrayList<de.ifgi.sc.smartcitiesapp.messaging.Message> ();
         mPubMessagesTest.add(new de.ifgi.sc.smartcitiesapp.messaging.Message("m_id2", "z_id", new Date(), 51.0, 7.0, new Date(new Date().getTime()+600000), "top", "tit", "msg", true));
         mPubMessagesTest.add(new de.ifgi.sc.smartcitiesapp.messaging.Message("m_id3", "z_id1", new Date(), 52.0, 8.0, new Date(new Date().getTime()+600000), "top1", "tit1", "msg1", true));
         mP2PManager.shareMessage(mPubMessagesTest);
-
+        */
 
         // in case of the notification about new retrieved msgs was clicked:
         if (savedInstanceState == null) {
@@ -259,9 +263,6 @@ public class MainActivity extends AppCompatActivity implements MessagesObtainedL
                 startActivityForResult(intentSettings, 1);
             }
         });
-
-        // add the MessagesObtainedListener to the UIMessageManager:
-        UIMessageManager.getInstance().setMessageObtainedListener(this);
 
         // Listener-Test-Button:
         Button btn_test = (Button) findViewById(R.id.btn_test);
